f94b7f4a693f549991a9e4f2754e92c42dadd202
abunuwas <joseharoperalta@gmail.com>
2017-11-08 19:41:46 +0000

    Add further configuration and fix style

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './app'
+
+import 'bulma/css/bulma.css'
+
+import { App } from './app'
 import router from './router'
 import store from './store'
 
