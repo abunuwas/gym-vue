f63c8e9f5ad58dcfdd2b166f3c3d836eba052d64
abunuwas <joseharoperalta@gmail.com>
2017-11-27 18:09:48 +0000

    Add update and delete mutations

--- src/app/sets/vuex/mutations.js
+++ src/app/sets/vuex/mutations.js
@@ -1,8 +1,17 @@
+import { Vue } from 'vuex'
 import { guid } from '../../../utils'
 
 export default {
   ADD_SET (state, payload) {
     let id = guid()
     state.sets[id] = Object.assign({ id: id }, payload.set)
+  },
+
+  UPDATE_SET (state, payload) {
+    state.sets[payload.set.id] = payload.set
+  },
+
+  DELETE_SET (state, payload) {
+    Vue.delete(state.set, payload.set.id)
   }
 }
