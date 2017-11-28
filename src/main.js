3dfeb9831fa5472786b80ddce526cf4554a1349d
abunuwas <joseharoperalta@gmail.com>
2017-11-28 07:49:33 +0000

    Add dependency localforage-startswith

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import localforage from 'localforage'
+require('localforage-startswith')
 
 import 'bulma/css/bulma.css'
 
