6c06b3981d7dd8bbd0684771b5d8d7e0f16e0012
abunuwas <joseharoperalta@gmail.com>
2017-11-30 19:30:39 +0000

    Add layout for budget-category create and update views

---
+++ src/app/budgets/components/CreateUpdateBudgetCategory.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="budget-category-create-edit-view">
+    <form class="form" @submit.prevent="processSave">
+      <div class="control is-horizontal">
+        <div class="control is-grouped">
+          <div class="control is-expanded">
+            <input type="text" v-model="budgetCategory.category">
+          </div>
+          <div class="control is-expanded">
+            $<input type="number" class="input" v-model="budgetCategory.budgeted">
+          </div>
+          <div class="control is-expanded">
+            {{ budgetCategory.spent }}
+          </div>
+          <button @click.prevent="processSave">Add</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  import { mapActions, mapGetters } from 'vuex'
+
+  export default {
+    name: 'budget-category-create-edit-view',
+
+    components: {
+    },
+
+    data: () => {
+      return {
+        budgetCategory: {}
+      }
+    },
+
+    mounted () {
+    },
+
+    methods: {
+      ...mapActions([
+      ]),
+
+      processSave () {
+      }
+    },
+
+    computed: {
+      ...mapGetters([
+      ])
+    }
+  }
+</script>
