0f473001a26658e792e53e6ee6067c45b0025b5a
abunuwas <joseharoperalta@gmail.com>
2017-11-30 17:41:17 +0000

    Add categories getter

--- src/app/budgets/vuex/getters.js
+++ src/app/budgets/vuex/getters.js
@@ -2,4 +2,8 @@ export default {
   getBudgetById: (state, getters) => (budgetId) => {
     return state.budgets && budgetId in state.budgets ? state.budgets[budgetId] : false
   }
+
+  getCategoryById: (state, getters) => (categoryId) => {
+    return state.categories && categoryId in state.categories ? state.categories[categoryId] : false
+  }
 }
