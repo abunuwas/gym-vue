bfe5ff33bb77c6416e92345e6f69e17b74fb6709
abunuwas <joseharoperalta@gmail.com>
2017-12-03 20:09:03 +0000

    Add core styling

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <div class="columns app-container">
+      <div class="column is-3 sidebar"></div>
+      <div class="column">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,6 +22,50 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  .columns.app-container {
+    flex: 1;
+    display: block;
+    padding: 0;
+    margin: 0;
+  }
+  @media only screen and (min-device-width : 769px) {
+    .columns.app-container {
+      display: flex
+    }
+  }
+  .columns.app-container:last-child {
+    margin-bottom: 0;
+  }
+  .sidebar {
+    background-color: #02212B;
+
+    .brand {
+      font-size: 36px;
+      color: #ffffff;
+      margin: 40px 0 20px 20px;
+      vertical-align: top;
+      img {
+        margin: 4px;
+        max-width: 24%;
+        display: inline;
+      }
+    }
+  }
+
+  .table td, .table th {
+    background-color: #bbbbbb;
+  }
+  tbody tr {
+    background-color: rgba(34, 109, 59, 0.2);
+  }
+  .is-delinquent {
+    background-color: rgba(148, 62, 46, 0.2);
   }
 </style>
